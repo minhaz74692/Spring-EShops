bd04a21ac1531821c52e9ea5b4d7f13c49761ac4
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-11-17 22:28:38 +0600

    initial commit

---
+++ src/main/java/com/dailycodework/myeshops/MyEshopsApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.myeshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEshopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEshopsApplication.class, args);
+	}
+
+}
